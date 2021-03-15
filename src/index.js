60053745c284f199ca13795d350e9665b1355a40
ereticor <ereticor@gmail.com>
2021-03-15 12:50:09 +0300

    feat: add task solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let x = [];
+  for (let i = 0; i < bracketsConfig.length;) {
+    x[i] = bracketsConfig[i].join("");
+    if (str.includes(x[i])) {
+      str = str.replace(x[i], "");
+      i = 0;
+    }
+    else {
+      i++;
+    }
+  }
+  // console.log(str.length);
+  if (str.length == 0) {
+    return true;
+  }
+  else {
+    return false;
+  }
 }
